4162a931776b930e700c673765f4e3417b97fb12
Dominic Bronnenkant <dominic.bronnenkant@meinautohaus.de>
2025-04-16 15:26:24 +0200

    remove staged files

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  tmdbApiKey: '8826281b83eb8f0fce15553129972f92',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  tmdbApiKey: '8826281b83eb8f0fce15553129972f92',
-};
